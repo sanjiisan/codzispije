99b0b4a28ae2dd8deb49281c7af90670cfaef7b5
Pawel Wojcik <pawel.wojcik@obido.pl>
2015-11-26 04:02:19 +0100

    Verion 1.0

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -13,14 +13,25 @@ class IndexController extends Controller
      */
     public function indexAction()
     {
+        return $this->render('@App/Page/index.html.twig');
+    }
+
+    /**
+     * @Route("/random", name="random")
+     */
+    public function randomAction()
+    {
         $manager = $this->get('doctrine.orm.entity_manager');
 
         $repo = $manager->getRepository(Drink::class);
 
         $drinks = $repo->findAll();
 
-        return $this->render('@App/Page/index.html.twig', array(
-            'drink' => $drinks[rand(0, count($drinks) - 1)]
-        ));
+        return $this->render('@App/Page/random.html.twig',
+            [
+                'drink' => $drinks[rand(0, count($drinks) - 1)]
+            ]
+        );
+
     }
 }
